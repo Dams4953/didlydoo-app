0dc16950cb14cce9f2105b4149e073ece3f49c1e
Damien <legat@CQJ>
2024-01-23 11:30:53 +0100

    bouton

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-document.getElementById('idBouton').addEventListener('click', async function (event) {
-    try {
-        
+document.getElementById('idBouton').addEventListener('click', function (event) {
 
-    } catch (error) {
-        alert('bouton error');
-    }
+    event.preventDefault();
+
+    // valeurs du formulaire stockées dans des variables
+    let eventName = document.getElementById('event-name').value;
+    let eventAuthor = document.getElementById('event-author').value;
+    let eventDate = document.getElementById('event-date').value;
+    let eventDescription = document.getElementById('event-description').value;
+
+    // création de la div après le click
+    let div = document.createElement('div');
+    div.innerHTML = `<p>Nom de l'événement : ${eventName}</p>
+                        <p>Auteur : ${eventAuthor}</p>
+                        <p>Date de l'événement : ${eventDate}</p>
+                        <p>Description : ${eventDescription}</p>`;
+    let main = document.querySelector('main');
+    main.appendChild(div);
+
+
+
+
 });
+
