f7f3f8650c70eeb6fb26c9a2850b529fc7e1898c
Damien <legat@CQJ>
2024-01-24 16:24:49 +0100

    ajout du GET et POST / ajout du DELETE

--- js/delete.js
+++ js/delete.js
@@ -1,9 +1,35 @@
-export function deleteItems() {
+// fonction qui va chercher l'api pour delete
+export async function deleteEvent(eventId) {
+    try {
+        const response = await fetch(`http://localhost:3000/api/events/${eventId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-    let bouton_supprimer = document.querySelectorAll('.idDiv__bouton-suppression');
-    bouton_supprimer.forEach(function (bouton) {
-        bouton.addEventListener('click', function () {
-            this.parentElement.remove();
+       
+    } catch (error) {
+        console.error('error fonction deleteEvent', error);
+    }
+}
+
+
+// function pour delete
+export function deleteItems(events) {
+    let boutonsSupprimer = document.querySelectorAll('.conteneur__idDiv__bouton-suppression');
+    boutonsSupprimer.forEach((bouton, index) => {
+        bouton.addEventListener('click', async function () {
+            try {
+                const eventId = events[index].id;
+
+                await deleteEvent(eventId);
+                
+                bouton.parentElement.remove();
+
+            } catch (error) {
+                console.error('Erreur lors de la suppression côté client', error);
+            }
         });
     });
 }
